481965f3be3db5a0d8926f9f85525843a1847774
Lexa <metall112@bk.ru>
2016-07-25 19:54:33 +0300

    #CreateUI start working //add files

---
+++ gitClient/NSDHomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  NSDHomeViewController.h
+//  gitClient
+//
+//  Created by NullStackDev on 25.07.16.
+//  Copyright © 2016 nullStackDev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NSDHomeViewController : UIViewController
+
+@end
---
+++ gitClient/NSDReposViewController.h
@@ -0,0 +1,13 @@
+//
+//  NSDReposViewController.h
+//  gitClient
+//
+//  Created by NullStackDev on 25.07.16.
+//  Copyright © 2016 nullStackDev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NSDReposViewController : UIViewController
+
+@end
