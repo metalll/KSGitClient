ffef52291d8c08567e3a56870adf004a3e2ef500
Lexa <metall112@bk.ru>
2016-07-27 10:48:29 +0300

    #fix

---
+++ gitClient/NSDUser.m
@@ -0,0 +1,13 @@
+//
+//  NSDUser.m
+//  gitClient
+//
+//  Created by NullStackDev on 26.07.16.
+//  Copyright © 2016 nullStackDev. All rights reserved.
+//
+
+#import "NSDUser.h"
+
+@implementation NSDUser
+
+@end
